6bdf9d724e8a5d539eafadd5115753e6c4f70049
Jun Ni <jun.ni@tanan.net.cn>
2018-03-01 07:55:27 +0800

    elfinder 7.x-2.x-dev
    Project elfinder contains 3 modules: elfinder_ffsel, elfinder_bueditor, elfinder.

---
+++ docs/sites/all/modules/contrib/elfinder/editors/ckeditor/ckeditor.callback.js
@@ -0,0 +1,31 @@
+// $Id$
+
+function elfinder_ckeditor_callback(arg1) {
+ // console.log(typeof url);
+
+  var url = arg1;
+
+  if (typeof arg1 == 'object') {
+    url = arg1.url;
+  }
+  funcNum = window.location.search.replace(/^.*CKEditorFuncNum=(\d+).*$/, "$1");
+  window.opener.CKEDITOR.tools.callFunction(funcNum, url, function() {
+    // adapted from http://docs.ckeditor.com/#!/guide/dev_file_browser_api
+    var dialog = this.getDialog();
+    if (dialog.getName() == 'link' ) {
+      var element = dialog.getContentElement('info', 'linkDisplayText');
+      var display_text = element.getValue();
+      // If display text is blank, insert the filename.
+      if (element && !display_text) {
+        element.setValue(arg1.name);
+      }
+    }
+  });
+
+  // Avoid beforeunload event when selecting an image.
+  // See https://github.com/Studio-42/elFinder/issues/1340
+  // Maybe remove this when elfinder js library gets updated.
+  $(window).off('beforeunload');
+
+  window.close();
+}
---
+++ docs/sites/all/modules/contrib/elfinder/modules/elfinder_ffsel/ffs.callback.js
@@ -0,0 +1,23 @@
+function elfinder_ffs_callback(arg1) {
+
+          var fieldName = Drupal.settings.elfinder.field_name;
+          var fieldId = Drupal.settings.elfinder.filepath_id;
+
+          var url = arg1;
+
+          if (typeof arg1 == 'object') {
+            url = arg1.url;
+          }
+
+          var filePath =  arg1.join('%%');
+
+          window.opener.jQuery('input#'+fieldId).val(filePath).change();
+          window.opener.focus();
+
+          // Avoid beforeunload event when selecting an image.
+          // https://github.com/Studio-42/elFinder/issues/1340
+          // Maybe remove this when elfinder js library gets updated.
+          $(window).off('beforeunload');
+
+          window.close();
+}
