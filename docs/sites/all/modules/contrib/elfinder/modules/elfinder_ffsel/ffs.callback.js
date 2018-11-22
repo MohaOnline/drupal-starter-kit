736d52c6afa1d8b68ad7ae3fe8abc07eede69805
Jun Ni <jun.ni@tanan.net.cn>
2018-11-22 11:46:45 +0800

    elfinder 7.x-2.x-dev > 7.x-2.x-dev

--- docs/sites/all/modules/contrib/elfinder/modules/elfinder_ffsel/ffs.callback.js
+++ docs/sites/all/modules/contrib/elfinder/modules/elfinder_ffsel/ffs.callback.js
@@ -5,20 +5,27 @@
  */
 
 function elfinder_ffs_callback(arg1) {
-
   var fieldName = Drupal.settings.elfinder.field_name;
   var fieldId = Drupal.settings.elfinder.filepath_id;
 
   var url = arg1;
 
   if (typeof arg1 == 'object') {
-    url = arg1.url;
-  }
+    if ($.isArray(arg1)) {
+      url = arg1[0];
+    } else {
+      url = arg1.url;
+    }
+  } 
 
   var filePath = url;
 
   /* Needs rework: must support both classic single file selection and multiple selection */
   //var filePath =  arg1.join('%%');
+
+  if (typeof filePath == 'undefined') {
+    console.log('Undefined filePath, please check integration');
+  }
 
   window.opener.jQuery('input#' + fieldId).val(filePath).change();
   window.opener.focus();
