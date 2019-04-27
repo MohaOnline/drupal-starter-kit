6fd3708a49cc01dd93ec6c56be813306bdaa4bd4
Jun Ni <jun.ni@tanan.net.cn>
2019-04-27 16:13:13 +0800

    uikit 7.x-3.4 > 7.x-3.5

--- docs/sites/all/themes/contrib/uikit/src/UIkit.php
+++ docs/sites/all/themes/contrib/uikit/src/UIkit.php
@@ -19,7 +19,7 @@ class UIkit {
    *
    * @var string
    */
-  const UIKIT_LIBRARY_VERSION = '3.0.1';
+  const UIKIT_LIBRARY_VERSION = '3.1.4';
 
   /**
    * The Drupal project page for the UIkit base theme.
