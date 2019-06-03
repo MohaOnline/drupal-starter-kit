ec478f0e9cb3c294fb6196e8cdc43f291acd5158
Jun Ni <jun.ni@tanan.net.cn>
2019-06-04 01:06:04 +0800

    akamai 7.x-2.2

---
+++ docs/sites/all/modules/contrib/akamai/akamai-page-cache-control-block.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file akamai-page-cache-control-form.tpl
+ * Default theme implementation for displaying an akamai cache clearing 
+ * form for a site page.
+ */
+?>
+<div id="akamai-cache-control-block">
+  FORM STARTS HERE
+  <?php print $cache_control_form; ?>
+  FORM ENDS HERE
+</div>
