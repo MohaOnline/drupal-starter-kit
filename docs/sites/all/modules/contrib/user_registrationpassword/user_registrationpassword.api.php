846cc9a2e20144d30f6d33c5b441293ab7b3aae7
Jun Ni <tweeg.ni@gmail.com>
2022-04-02 00:23:04 +0800

    user_registrationpassword 7.x-1.5

---
+++ docs/sites/all/modules/contrib/user_registrationpassword/user_registrationpassword.api.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Hooks for the user_registrationpassword module.
+ */
+
+/**
+ * This hook is called after a user activates their account.
+ */
+function hook_user_registrationpassword_activated_user($account) {
+}
