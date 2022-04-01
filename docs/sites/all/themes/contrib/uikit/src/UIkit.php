2b8ff74346738b83933b57ce97f3c66de97afe64
Jun Ni <tweeg.ni@gmail.com>
2022-04-01 23:56:42 +0800

    uikit 7.x-3.9 > 7.x-3.10

--- docs/sites/all/themes/contrib/uikit/src/UIkit.php
+++ docs/sites/all/themes/contrib/uikit/src/UIkit.php
@@ -19,7 +19,7 @@ class UIkit {
    *
    * @var string
    */
-  const UIKIT_LIBRARY_VERSION = '3.2.0';
+  const UIKIT_LIBRARY_VERSION = '3.5.9';
 
   /**
    * The Drupal project page for the UIkit base theme.
